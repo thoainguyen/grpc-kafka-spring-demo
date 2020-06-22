0ceeb44643ccf247d659dd610daf08ac470f9e4b
Thoai. Nguyen Huynh <thoainh@vng.com.vn>
2020-06-22 17:29:25 +0700

    Demo cucumber and integrate allure report in jenkins

--- src/test/java/com/bkteam/RunCucumberTest.java
+++ src/test/java/com/bkteam/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"})
 public class RunCucumberTest {
 }
